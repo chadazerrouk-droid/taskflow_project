22e8c74560fe511a03adf5234b66d74434ce1c8b
chada ZERROUK <chada.zerrouk@etu.uae.ac.ma>
2026-05-06 22:32:30 +0100

    feat: integrate dashboard routes into server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes'); // ← AJOUT
 
 dotenv.config();
 connectDB();
@@ -26,6 +27,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
+app.use('/api/dashboard', dashboardRoutes); // ← AJOUT
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
