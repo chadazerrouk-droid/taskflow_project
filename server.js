3863e005e69c80339309445499578e4031328962
Chakron Maroua <maroua.chakron@etu.uae.ac.ma>
2026-05-11 13:21:44 +0100

    feat: dashboard backend + front avec tri priorite/date

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const auth = require('./middleware/auth');
 const tasksRoutes = require('./routes/tasks');
 
@@ -12,6 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/tasks', tasksRoutes);
