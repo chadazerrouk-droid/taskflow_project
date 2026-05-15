e056ef9e128b7919c5bff8fba0458edf5ef50c54
chada ZERROUK <chada.zerrouk@etu.uae.ac.ma>
2026-05-15 15:06:45 +0100

    update

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/Task');
+const Task = require('../models/task');
 const Project = require('../models/Project');
 const auth = require('../middleware/auth');
 const User = require('../models/User');
