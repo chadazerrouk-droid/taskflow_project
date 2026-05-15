149c541f9df8513daf24009e3b66612ce8e4a53a
basma-dev-stack <tonemailgmail.com>
2026-05-15 23:17:52 +0100

    fix: use protect function from authMiddleware

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/authMiddleware');
+const auth = require("../middleware/authMiddleware").protect;
 const {
   getActivities
 } = require('../controllers/activityController');
--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/authMiddleware');
+const { protect: auth } = require("../middleware/authMiddleware").protect;
 const {
   inviteMember,
   removeMember,
