5f6c583f6609bee7d36dcdf9ecb9f52bb86eafbb
chada ZERROUK <chada.zerrouk@etu.uae.ac.ma>
2026-05-15 16:39:28 +0100

    chore: remove temporary taskRoutes.js (will be added by E3)

--- routes/taskRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Tasks endpoint (temporaire)' });
-});
-
-module.exports = router;
