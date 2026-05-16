7ef2a3f2c783644fc85b94128597364131f790f4
zainab <zainabelkharraz6@gmail.com>
2026-05-16 18:42:51 +0100

    correction des fichiers

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,47 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/authMiddleware");
+const { getNotifications, createNotification, markAsRead } = require("../controllers/notificationController");
 
-const Notification = require("../models/Notification");
+router.get("/", auth, getNotifications);
+router.post("/", auth, createNotification);
+router.patch("/:id/read", auth, markAsRead);
 
-
-router.get("/", auth ,async (req, res) => {
-    try {
-        const notifications = await Notification.find().sort({ createdAt: -1 });
-        res.json(notifications);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-
-router.post("/", async (req, res) => {
-    try {
-        const notif = new Notification({
-            message: req.body.message || "Test notification"
-        });
-
-        await notif.save();
-
-        res.json({
-            message: "Notification créée",
-            notif
-        });
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-router.patch("/:id/read", auth, async(req,res) =>{
-    try{
-        const notification = await Notification.findByIdAndUpdate(
-            req.params.id,
-            {read: true },
-            {new: true }
-        );
-        res.json(notification);
-    }catch(error){
-        res.status(500).json({ error: error.message });
-    }
-});
 module.exports = router;
