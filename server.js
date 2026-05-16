73ae09daf891c865dc5318c1d04b99744277b97d
zainab <zainabelkharraz6@gmail.com>
2026-05-16 20:46:05 +0100

    fix: resolve server.js conflicts

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-global.crypto = require("crypto");
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
+global.crypto = require('crypto');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
-<<<<<<< HEAD
 const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const memberRoutes = require('./routes/memberRoutes');
-//const activityRoutes = require('./routes/activityRoutes');
+const activityRoutes = require('./routes/activityRoutes');
 const tasks = require('./routes/tasks');
-//const dashboardRoutes = require('./routes/dashboardRoutes');
 const notificationRoutes = require('./routes/notificationRoutes');
-=======
-const authRoutes = require("./routes/authRoutes");
-const projectRoutes = require("./routes/projectRoutes");
-const memberRoutes = require("./routes/memberRoutes");
-const activityRoutes = require("./routes/activityRoutes");
-const tasks = require("./routes/tasks");
-// const dashboardRoutes = require('./routes/dashboardRoutes');
->>>>>>> 40ffc933ce26fcb1ea118c0876655dad1e7e54b7
 
 dotenv.config();
 connectDB();
@@ -27,36 +17,22 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('API TaskFlow est en ligne ');
+  res.send('API TaskFlow est en ligne 🚀');
 });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/projects', memberRoutes);
 app.use('/api/projects', activityRoutes);
+app.use('/api/projects/:projectId/tasks', tasks);
 app.use('/api/tasks', tasks);
-app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/notifications', notificationRoutes);
-=======
-app.get("/", (req, res) => {
-  res.send("API TaskFlow est en ligne 🚀");
-});
 
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/projects", memberRoutes);
-app.use("/api/projects", activityRoutes);
-app.use("/api/projects/:projectId/tasks", tasks);
-app.use("/api/tasks", tasks);
-// app.use('/api/dashboard', dashboardRoutes);
-
-
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK" });
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'OK' });
 });
 
 const PORT = process.env.PORT || 3000;
