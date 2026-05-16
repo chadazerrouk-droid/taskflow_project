ee5529e3d0284dc86c8bd6dd522a4d98325cad87
chada ZERROUK <chada.zerrouk@etu.uae.ac.ma>
2026-05-16 18:17:04 +0100

    feat: update server.js configuration

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const projectRoutes = require('./routes/projectRoutes');
 const memberRoutes = require('./routes/memberRoutes');
 const activityRoutes = require('./routes/activityRoutes');
 const tasks = require('./routes/tasks');
-const dashboardRoutes = require('./routes/dashboardRoutes');
+//const dashboardRoutes = require('./routes/dashboardRoutes');
 const notificationRoutes = require('./routes/notificationRoutes');
 
 connectDB();
@@ -28,7 +28,7 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/projects', memberRoutes);
 app.use('/api/projects', activityRoutes);
 app.use('/api/tasks', tasks);
-app.use('/api/dashboard', dashboardRoutes);
+//app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/notifications', notificationRoutes);
 
 app.get('/api/health', (req, res) => {
