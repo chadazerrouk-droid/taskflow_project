0289965debec81461535d01fe07a0c82f36ee6e4
marouamahrach-hub <maroua.mahrach@etu.uae.ac.ma>
2026-05-16 19:35:05 +0100

    fix:modification server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-global.crypto = require('crypto');
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+global.crypto = require("crypto");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
-const authRoutes = require('./routes/authRoutes');
-const projectRoutes = require('./routes/projectRoutes');
-const memberRoutes = require('./routes/memberRoutes');
-const activityRoutes = require('./routes/activityRoutes');
-const tasks = require('./routes/tasks');
+const authRoutes = require("./routes/authRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const memberRoutes = require("./routes/memberRoutes");
+const activityRoutes = require("./routes/activityRoutes");
+const tasks = require("./routes/tasks");
 // const dashboardRoutes = require('./routes/dashboardRoutes');
 
 dotenv.config();
@@ -17,21 +17,22 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('API TaskFlow est en ligne 🚀');
+app.get("/", (req, res) => {
+  res.send("API TaskFlow est en ligne 🚀");
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/projects', memberRoutes);
-app.use('/api/projects', activityRoutes);
-app.use('/api/tasks', tasks);
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/projects", memberRoutes);
+app.use("/api/projects", activityRoutes);
+app.use("/api/projects/:projectId/tasks", tasks);
+app.use("/api/tasks", tasks);
 // app.use('/api/dashboard', dashboardRoutes);
 
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'OK' });
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK" });
 });
 
 const PORT = process.env.PORT || 3000;
